467eef9868b368ed20c56d07ba17921b72a31e04
Lyubomir Linev <lubomir_linev@yahoo.com>
2022-01-22 19:05:46 +0100

    Update root seed

--- root_jobs_seed.groovy
+++ root_jobs_seed.groovy
@@ -1,48 +1,18 @@
-folder('devops') {
-    displayName('DevOps')
-    description('DevOps jobs')
+folder('devops_project') {
+    displayName('Devops Project App')
+    description('Devops Project App')
 }
 
-job('devops/generate_jobs') {
+job('devops_project/generate_jobs') {
     displayName("Generate Jobs")
     parameters {
-        stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
+    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
     }
     scm {
         git {
             remote {
-                github('Constructor-io/constructor_devops')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('jenkins/jobs/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('autocomplete') {
-    displayName('Autocomplete')
-    description('Autocomplete related jobs')
-}
-
-job('autocomplete/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-        stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/autocomplete')
-                credentials('constructorbot_github_token')
+                github('l-linev/devops-project')
+                credentials('GitHub')
             }
             branch('$GIT_REF')
         }
@@ -58,275 +28,25 @@ job('autocomplete/generate_jobs') {
     }
 }
 
-folder('pypi_repositories') {
-    displayName('PyPi Repositories')
-    description('PyPi Repositories')
-}
-
-folder('pypi_repositories/cnstrc_tokenizer') {
-    displayName('cnstrc_tokenizer')
-    description('cnstrc_tokenizer')
-}
-
-multibranchPipelineJob('pypi_repositories/cnstrc_tokenizer/ci-cd') {
+multibranchPipelineJob('devops_project/ci-cd') {
     displayName("CI-CD")
     branchSources {
         github {
-            id('47101153232') // IMPORTANT: use a constant and unique identifier
+            id('631742818912') // IMPORTANT: use a constant and unique identifier
             buildForkPRHead(false)
             buildForkPRMerge(true)
             buildOriginBranch(false)
             buildOriginBranchWithPR(false)
             buildOriginPRHead(false)
             buildOriginPRMerge(true)
-            scanCredentialsId('constructorbot_github_token')
-            repoOwner('Constructor-io')
-            repository('cnstrc_tokenizer')
+            scanCredentialsId('GitHub')
+            repoOwner('l-linev')
+            repository('devops-project')
         }
     }
     orphanedItemStrategy {
         discardOldItems {
             numToKeep(20)
-        }
-    }
-}
-
-folder('customer-docs') {
-    displayName('Customer Docs')
-    description('Customer Docs')
-}
-
-job('customer-docs/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-        stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/customer-docs-v2')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('deploy/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('dice') {
-    displayName('Dice App')
-    description('Dice App related jobs')
-}
-
-job('dice/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/dice-experiment-analysis-tool')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('deploy/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('cubejs') {
-    displayName('Cubejs App')
-    description('Cubejs App related jobs')
-}
-
-job('cubejs/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/analytics-service')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('deploy/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('clickhouse') {
-    displayName('Clickhouse')
-    description('Clickhouse jobs')
-}
-
-job('clickhouse/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/constructor_devops')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('jenkins/jobs/clickhouse/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('data_pipeline_v2') {
-    displayName('Data Pipeline v2')
-    description('Data Pipeline related jobs')
-}
-
-job('data_pipeline_v2/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/data_pipeline')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('jenkins_jobs/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('eng_org') {
-    displayName('Engineering Organization')
-    description('Engineering Organization WebApp')
-}
-
-job('eng_org/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/eng-org')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('deploy/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('quizzes') {
-    displayName('Quizzes')
-    description('Quiz Service related jobs')
-}
-
-job('quizzes/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/quiz-service')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('deploy/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
-        }
-    }
-}
-
-folder('website_constructor') {
-    displayName('Website Constructror.io')
-    description('New Website Constructror.io jobs')
-}
-
-job('website_constructor/generate_jobs') {
-    displayName("Generate Jobs")
-    parameters {
-    stringParam('GIT_REF', 'master', 'GIT reference, branch, tag or commit')
-    }
-    scm {
-        git {
-            remote {
-                github('Constructor-io/website')
-                credentials('constructorbot_github_token')
-            }
-            branch('$GIT_REF')
-        }
-    }
-    steps {
-        dsl {
-            external('deploy/jobs_seed.groovy')
-            lookupStrategy('SEED_JOB')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-            additionalClasspath('lib')
         }
     }
 }
