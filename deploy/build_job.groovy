3080fd818711c0d1a9ac4c9a2c2bb94a5840181a
Lyubomir Linev <lubomir_linev@yahoo.com>
2022-01-22 21:49:50 +0100

    Added Mode param for Environment

--- deploy/build_job.groovy
+++ deploy/build_job.groovy
@@ -8,6 +8,7 @@ pipeline {
         booleanParam(name: 'PUSH_DOCKER_IMAGES', defaultValue: false, description: 'Push docker images')
         booleanParam(name: 'TEST_MERGE_COMMIT', defaultValue: false, description: 'Tries merging with origin/master and runs test for that commit.')
         string(name: 'GIT_REF', defaultValue: 'main', description: 'Project repository GIT REF')
+        choice(name: 'MODE', choices: ['dev', 'prod', 'beta', 'staging'], description: 'Environment mode')
     }
     options {
         timeout(time: 30, unit: 'MINUTES')
@@ -52,7 +53,7 @@ pipeline {
             steps {
                 script {
                     docker.withRegistry('https://914194858346.dkr.ecr.us-east-1.amazonaws.com') {
-                        env.devops_project_docker_tag = "dev-${env.BUILD_ID}"
+                        env.devops_project_docker_tag = "${params.MODE}-${env.BUILD_ID}"
                         devops_project_image = docker.build("914194858346.dkr.ecr.us-east-1.amazonaws.com/devops_project:${env.devops_project_docker_tag}", ".")
                     }
                 }
